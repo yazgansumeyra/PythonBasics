94d7e37f63326dad6e5863e6dbac2877ad00814b
Sumeyra Yazgan <Sumeyra@Sumeyras-MacBook-Air.local>
2019-10-17 22:18:03 +0100

    removed code

--- firstScript.py
+++ firstScript.py
@@ -1,11 +1,6 @@
 import requests
 
-print("hello")
 
-# r = requests.get("https://coreyms.com")
-# print(r.status_code)
-
-name = input("your name? ")
-print("name :", name)
-print("1")
+r = requests.get("https://coreyms.com")
+print(r.status_code)
 
